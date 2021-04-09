f5b3533a1b027a70be0ab9980dc717a0a748f844
Javiya Deep <patelaleis@gmail.com>
2021-04-09 18:06:53 +0530

    Update

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,4 +1,8 @@
 $(function(){
     // Feather Icon Init Js
     feather.replace();
+
+    $(".toggle-minimize").on("click", function(){
+        $(".sidebar").toggleClass("show-sidebar");
+    });
 });
