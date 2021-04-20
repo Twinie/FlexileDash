bbe810fe70d317930ed37dece9ea10d6f2c70723
Javiya Deep <patelaleis@gmail.com>
2021-04-20 18:03:24 +0530

    Update Chart

--- dist/js/monthly-chart.js
+++ dist/js/monthly-chart.js
@@ -1,45 +1,33 @@
 var options = {
+    colors: ['#1a9bfc', '#080'],
+
     series: [{
         name: 'Monthly Sales',
         data: [44, 83, 25, 72, 38]
     }],
 
-    colors: ['#1a9bfc', '#080'],
-
-    chart: {
-        height: 83,
-        width: '100%',
-        offsetY: 0,
-        type: 'area',
-
-        toolbar: {
-            show: false,
-        },
-
-        sparkline: {
-            enabled: true,
-        },
-
-    },
-
-
-    dataLabels: {
-        enabled: false
-    },
-
+    
     fill: {
         colors: '#f3faff',
         opacity: 1,
         type: 'solid',
     },
 
-    stroke: {
-        curve: 'smooth'
+
+    chart: {
+        type: 'area',
+        height: 83,
+        width: '100%',
+
+        sparkline: {
+            enabled: true,
+        },
+
+        toolbar: {
+            show: false,
+        },
     },
 
-    grid: {
-        show: false,
-    },
 };
 
 var chart = new ApexCharts(document.querySelector("#monthly-sales"), options);
